fe4669a49c487c0c18e63d616bb0730e66e97571
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-30 10:24:26 +0300

    feat: add divide function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,8 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
+function divide(num1, num2) {
+  if (num2 === 0) return "can't divide by 0";
+  return num1 / num2;
+}
