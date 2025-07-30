4a8b06396648afe38c6cf0a0c2b7e988f4bba6dd
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-30 10:44:18 +0300

    feat: add operate() function to perform basic arithmetic operations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const OPERATOR = ["*", "/", "+", "-"];
-let num1;
-let num2;
+let preValue;
+let curValue;
 let result;
 
 function add(num1, num2) {
@@ -18,4 +18,17 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   if (num2 === 0) return "can't divide by 0";
   return num1 / num2;
+}
+
+function operate(num1, ope, num2) {
+  switch (ope) {
+    case `*`:
+      return multiply(num1, num2);
+    case `/`:
+      return divide(num1, num2);
+    case `+`:
+      return add(num1, num2);
+    case `-`:
+      return subtract(num1, num2);
+  }
 }
